8cf2f4cb2ed49b3f4e2d07a1786ec99868418df1
Jesusmaster <armandodejesus678@gmail.com>
2020-01-23 17:38:36 -0600

    client entry point created

--- source/client/index.js
+++ source/client/index.js
@@ -1,1 +1,13 @@
-console.log('working...')
+import React, { hydrate } from 'react-dom'
+import App from '../app'
+import { BrowserRouter } from 'react-router-dom'
+
+function Client () {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
+}
+
+hydrate(<Client />, document.getElementById('render_targte'))
