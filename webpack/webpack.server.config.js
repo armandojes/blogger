3f91357195add1d3cf9c0820d5589aab20ef982e
Jesusmaster <armandodejesus678@gmail.com>
2020-01-23 12:43:00 -0600

    initial config webpack

---
+++ webpack/webpack.server.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const webpack = require('webpack');
+const extract = require('mini-css-extract-plugin');
+
+const config = {
+  entry: ["regenerator-runtime/runtime", path.resolve(__dirname, '../source/server/index.js')],
+  output: {
+    path: path.resolve(__dirname, '../build'),
+    filename: 'server.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: "/(node_modules)/",
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react',
+          ],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { 
+            loader: extract.loader,
+          }, 
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+    alias: {
+      hooks: path.resolve(__dirname, '../source/app/hooks'),
+      helpers: path.resolve(__dirname, '../source/app/helpers'),
+      components: path.resolve(__dirname, '../source/app/components'),
+    }
+  },
+  plugins: [
+    new extract({
+      filename: "styles.css"
+    }),
+    new webpack.DefinePlugin({
+      IS_PRODUCTION: process.env.NODE_ENV === 'production' ? true : false,
+      URL: JSON.stringify(process.env.NODE_ENV === 'production' ? 'http://localhost:3000' : 'http://localhost:3000'),
+      ASSETS: JSON.stringify(process.env.NODE_ENV === 'production' ? 'http://localhost:3000/public' : 'http://localhost:3000/public'),
+    })
+  ],
+  target: 'node',
+}
+
+
+module.exports = config;
