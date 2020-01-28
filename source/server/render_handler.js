817ecded36f5544be2e2d8276f6120739b888005
Jesusmaster <armandodejesus678@gmail.com>
2020-01-28 16:42:09 -0600

    add redux on server render

--- source/server/render_handler.js
+++ source/server/render_handler.js
@@ -3,14 +3,32 @@ import marcup from './markup'
 import { renderToStringAsync } from 'react-fetch-ssr'
 import { StaticRouter } from 'react-router'
 import App from '../app'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import ReduxThunk from 'redux-thunk'
+import reducer from 'flux/reducer'
 
 async function renderHandler (request, response) {
-  const { content, states } = await renderToStringAsync(
-    <StaticRouter location={request.url}>
-      <App />
-    </StaticRouter>
+  // create store
+  const store = createStore(
+    reducer,
+    applyMiddleware(
+      ReduxThunk
+    )
   )
-  const fullHtml = marcup(content, states)
+
+  // render
+  const { content, states } = await renderToStringAsync(
+    <Provider store={store}>
+      <StaticRouter location={request.url}>
+        <App />
+      </StaticRouter>
+    </Provider>
+  )
+
+  // getState
+  const reduxPreloadedState = store.getState()
+  const fullHtml = marcup(content, states, reduxPreloadedState)
   response.send(fullHtml)
 }
 
